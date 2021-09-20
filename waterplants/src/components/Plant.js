e3d60abe8dc34a4256eb33e0ccf3e46932bc4f94
MatthewMion <87820540+ShmoneJones@users.noreply.github.com>
2021-09-20 15:00:40 -0400

    prototype  my-plant component

---
+++ waterplants/src/components/Plant.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+//values are hardcoded temporarily
+
+const Plant = () => {
+  return (
+    <div>
+      <h3>Plant Name:</h3>
+      <h6>Plant Species:</h6>
+      <h6>Water Frequency:</h6>
+
+      <button>Update</button>
+      <button>Delete</button>
+    </div>
+  );
+};
+
+export default Plant;
