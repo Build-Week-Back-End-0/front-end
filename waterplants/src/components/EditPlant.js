65afc19a2c29c24221fd350b1a9d6497c56cc679
MatthewMion <87820540+ShmoneJones@users.noreply.github.com>
2021-09-21 14:26:18 -0400

    working on EditPlant

--- waterplants/src/components/EditPlant.js
+++ waterplants/src/components/EditPlant.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const EditPlant = () => {
+  const [editPlant, setEdit] = useState();
   return (
     <div>
       <form>
